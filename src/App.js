b9d0d6d6c799505414c084ef9dfe6c15e635ba1b
Ayush Kamboj <aayush.kamboj@stackera.io>
2023-10-09 16:51:04 +0530

    Layout changes in the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 import "./App.css";
 import { SideNav } from "./components/sideNav/sidenav";
+import { Main } from "./components/main/main";
+import { Flex } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <SideNav />
+      <Flex>
+        <SideNav />
+        <Main />
+      </Flex>
     </div>
   );
 }
