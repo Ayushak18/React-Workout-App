a828c93591ecd27535f271f36bf2ffebc2185ec3
Ayush Kamboj <aayush.kamboj@stackera.io>
2023-10-09 16:51:41 +0530

    Main section of the applicaiton is added

---
+++ src/components/main/main.jsx
@@ -0,0 +1,19 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const Main = () => {
+  return (
+    <>
+      <Box w={"75%"} p={"10"}>
+        <Text
+          textAlign={"center"}
+          color={"  #2982cc "}
+          fontSize={"42px"}
+          fontWeight={"500"}
+          borderBottom={"1px solid   #2982cc "}
+        >
+          Enter your information
+        </Text>
+      </Box>
+    </>
+  );
+};
